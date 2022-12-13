b161f3d02ee994a227097b794931cb732ce39470
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2022-12-13 21:52:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  //converting word to its reversed state
+  const wordReverse = word.split("").reverse().join("").toLowerCase()
+
+  //comparing if received word and previous word are alike then returning true or false
+  if (wordReverse.toLowerCase() === word) {
+    return true;
+  }else{
+    return false;
+  }
+
 }
+
+/*
+  Need to make a function `isPalindrome` that returns true if the input string is the same after I 
+  reverse it and false  if the input string is not the same word after I reverse it. Exapmple, "mom"` in reverse is also
+"mom"`, and `"racecar"` in reverse is also `"racecar"`, so my solution should return a boolean true.
+*/
 
 /* 
   Add your pseudocode here
+  
+  Assign a variable, then check if the word is equal to its reversed state,
+    all in lowercase.
+  If the word is same;
+      return true
+  if word is not the same, return false
 */
+
 
 /*
   Add written explanation of your solution here
+  1. Convert the string to its reverse word
+  2. Make all be of the same case, lowercase.
+  3. Check if conditions are true.
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +47,30 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("refer"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("rotor"));
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("deed"));
+
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("peep"));
+
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("noon"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("cat"));
+
+
+
+
 }
 
 module.exports = isPalindrome;
